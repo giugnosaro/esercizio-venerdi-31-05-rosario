2205b011fd1010105702836e988d60a58f5a2aef
giugnosaro <giugno_rosario@libero.it>
2024-05-31 16:17:25 +0200

    sesto

--- esercizio/src/app/components/utenti/utenti.component.ts
+++ esercizio/src/app/components/utenti/utenti.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Utenti } from '../../models/utenti';
+import { UtentiService } from '../../utenti.service';
+import { TodosService } from '../../todos.service';
+import { Todos } from '../../models/todos';
 
 @Component({
   selector: 'app-utenti',
   templateUrl: './utenti.component.html',
   styleUrl: './utenti.component.scss'
 })
-export class UtentiComponent {
+export class UtentiComponent implements OnInit{
+  utenti: Utenti[] = [];
+  todos: any[] = [];
 
+  constructor(private utentiService: UtentiService, private todosService: TodosService ) {}
+  ngOnInit(): void {
+    this.utentiService.getAllUtenti().subscribe((utenti: Utenti[]) => {
+      console.log('Utenti:', utenti);
+      this.utenti = utenti;
+    });
+
+    this.todosService.getAllTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+  getTodosByUserId(userId: number): Todos[] {
+    return this.todos.filter(todo => todo.userId === userId);
+  }
+
+  isCompleted(userId: number): boolean {
+    const todosByUser = this.getTodosByUserId(userId);
+    return todosByUser.every(todo => todo.completed);
+  }
 }
